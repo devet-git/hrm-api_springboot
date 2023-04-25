bed9a8fd0a49cf6dc85da8b232b8c0533a9ab778
devet-git <devet.279@gmail.com>
2023-04-25 12:58:50 +0700

    update department

--- src/main/java/com/intern/hrmanagementapi/repo/DepartmentRepo.java
+++ src/main/java/com/intern/hrmanagementapi/repo/DepartmentRepo.java
@@ -1,6 +1,8 @@
 package com.intern.hrmanagementapi.repo;
 
 import com.intern.hrmanagementapi.entity.DepartmentEntity;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DepartmentRepo extends JpaRepository<DepartmentEntity, UUID> {
 
+  List<DepartmentEntity> findAllByUserId(UUID userId);
+
+  Optional<DepartmentEntity> findByIdAndUserId(UUID id, UUID userId);
 }
